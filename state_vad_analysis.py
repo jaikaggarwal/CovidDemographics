fdacfb59a08c0dde267dc9dad0d6f0dde9e316e4
jaikaggarwal <jaikaggarwal@gmail.com>
2020-05-18 21:23:08 -0400

    finished, ready for automation

--- state_vad_analysis.py
+++ state_vad_analysis.py
@@ -23,10 +23,10 @@ class VadAnalysis:
     def __init__(self):
         self.valence_model, self.arousal_model, self.dominance_model = self.init_reg_models()
 
-    def init_reg_models(self):
 
+    def init_reg_models(self):
         stored_files = os.listdir(STORAGE_DIR)
-        df_vad = pd.read_csv('Vad Lexicon/lexicon.txt', delimiter='\t', header=0)
+        df_vad = pd.read_csv(STORAGE_DIR + 'lexicon.txt', delimiter='\t', header=0)
         df_vad = df_vad.dropna()
         df_vad.index = df_vad['Word']
         df_vad = df_vad[['Valence', 'Arousal', 'Dominance']]
@@ -68,42 +68,27 @@ class VadAnalysis:
             with open(STORAGE_DIR + "dominance_model.pkl", "wb") as file:
                 pickle.dump(dominance_model, file)
 
-
-        # if 'arousal_model.pkl' in stored_files:
-        #     arousal_model = pickle.load(STORAGE_DIR + "arousal_model.pkl")
-        # else:
-        #     arousal_model = self.fit_beta_reg(arousal, vad_embeddings)
-        #
-        # if 'dominance_model.pkl' in stored_files:
-        #     dominance_model = pickle.load(STORAGE_DIR + "dominance_model.pkl")
-        # else:
-        #     dominance_model = self.fit_beta_reg(dominance, vad_embeddings)
-
         self.goodness_of_fit(valence_model, valence, vad_embeddings)
         self.goodness_of_fit(arousal_model, arousal, vad_embeddings)
         self.goodness_of_fit(dominance_model, dominance, vad_embeddings)
 
-        return
+        return valence_model, arousal_model, dominance_model
 
     def fit_beta_reg(self, y, X):
         binom_glm = sm.GLM(y, X, family=sm.families.Binomial())
         binom_results = binom_glm.fit()
         return binom_results
 
-
     def goodness_of_fit(self, model, true, X):
         y_predicted = model.get_prediction(X)
         pred_vals = y_predicted.summary_frame()['mean']
         print(r2_score(true, pred_vals))
 
-
     def sentence_tokenize(self, text):
         return sent_tokenize(text)
 
-
     def none_or_empty(self, text):
         return text is None or len(text) == 0 or text == "[removed]" or text == '[deleted]'
-
 
     def get_dates(self, latest_date):
         # Get all week increments from earliest date until present
@@ -118,12 +103,10 @@ class VadAnalysis:
         epochs.append(int(latest_date.timestamp()))
         return dates, epochs
 
-
     def get_file_data(self, dirname, file_name):
         with open(f'{dirname}{file_name}', 'r', encoding="UTF-8") as file:
             data = file.readlines()
             return [line.strip() for line in data]
-
 
     def sort_files(self, dirname, cutoff=1):
         sort_map = {}
@@ -143,31 +126,71 @@ class VadAnalysis:
             sorted_files.extend(sort_map[i])
         return sorted_files
 
+    def get_post_score(self, encoded_sen):
+        #         sen_split = sentence_tokenize(text)
+        # encoded_sen = model.encode(text)
+        valence_pred_results = self.valence_model.get_prediction(encoded_sen)
+        valence_pred_means = valence_pred_results.predicted_mean
+        arousal_pred_results = self.arousal_model.get_prediction(encoded_sen)
+        arousal_pred_means = arousal_pred_results.predicted_mean
+        dominance_pred_results = self.dominance_model.get_prediction(encoded_sen)
+        dominance_pred_means = dominance_pred_results.predicted_mean
+        return valence_pred_means, arousal_pred_means, dominance_pred_means
 
     def get_week_score(self, dirname, week_file):
         # Get data stored for subreddit
+        if f'{week_file}.pkl' in os.listdir(STORAGE_DIR):
+            with open(STORAGE_DIR + f'{week_file}.pkl', "rb") as file:
+                week_embeddings = pickle.load(file)
+                print("USE SAVED WEEK EMBEDDINGS")
+        else:
+            # arousal_model = self.fit_beta_reg(arousal, vad_embeddings)
+            data = self.get_file_data(dirname, week_file)
+            valid_text = []
+            for element in data:
+                valid_text.extend(sentence_tokenize(element))
+            week_embeddings = model.encode(valid_text)
+            with open(STORAGE_DIR + f'{week_file}.pkl', "wb") as file:
+                pickle.dump(week_embeddings, file)
 
-        data = self.get_file_data(dirname, week_file)
-        all_means = np.array([])
+        v_scores, a_scores, d_scores = get_post_score(week_embeddings)
 
-        def get_post_score(text):
-            #         sen_split = sentence_tokenize(text)
-            encoded_sen = model.encode(text)
-            valence_pred_results = valence_model.get_prediction(encoded_sen)
-            valence_pred_means = valence_pred_results.predicted_mean
-            arousal_pred_results = arousal_model.get_prediction(encoded_sen)
-            arousal_pred_means = arousal_pred_results.predicted_mean
-            dominance_pred_results = dominance_model.get_prediction(encoded_sen)
-            dominance_pred_means = dominance_pred_results.predicted_mean
-            return valence_pred_means, arousal_pred_means, dominance_pred_means
-
-        valid_text = []
-        for element in data:
-            valid_text.extend(sentence_tokenize(element))
-        v_scores, a_scores, d_scores = get_post_score(valid_text)
         print(len(valid_text) == len(v_scores) == len(a_scores) == len(d_scores))
         return np.mean(v_scores), np.mean(a_scores), np.mean(d_scores), np.std(v_scores), np.std(a_scores), np.std(d_scores)
 
+    def run(self):
+        for i in range(3, 4):
+            print(f"Region {i}")
+            dirname = f"{STORAGE_DIR}Region{i}/"
+            sorted_files = sort_files(dirname, cutoff=11)
+            valence_means = []
+            valence_stds = []
+            arousal_means = []
+            arousal_stds = []
+            dominance_means = []
+            dominance_stds = []
+            for file in sorted_files:
+                print(file)
+                vmean, amean, dmean, vstd, astd, dstd = get_week_score(dirname, file)
+                valence_means.append(vmean)
+                valence_stds.append(vstd)
+                arousal_means.append(amean)
+                arousal_stds.append(astd)
+                dominance_means.append(dmean)
+                dominance_stds.append(dstd)
+
+            with open("valence_means.pkl", "wb") as file:
+                pickle.dump(valence_means, file)
+            with open("arousal_means.pkl", "wb") as file:
+                pickle.dump(arousal_means, file)
+            with open("dominance_means.pkl", "wb") as file:
+                pickle.dump(dominance_means, file)
+            with open("valence_stds.pkl", "wb") as file:
+                pickle.dump(valence_stds, file)
+            with open("arousal_means.pkl", "wb") as file:
+                pickle.dump(arousal_stds, file)
+            with open("dominance_means.pkl", "wb") as file:
+                pickle.dump(dominance_stds, file)
 
 if __name__ == "__main__":
     if model.device.type != "cuda":
